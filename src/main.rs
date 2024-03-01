23699a3cec54649eeeb2dd13b28fdc03df2e74b7
ducmanh1101 <manhnguyen@descartes.network>
2024-03-01 09:48:32 +0700

    fix ip

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpServer, Responder};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -7,23 +7,21 @@ struct Numbers {
     num2: i32,
 }
 
+#[post("/add")]
 async fn add(numbers: web::Json<Numbers>) -> impl Responder {
     let result = numbers.num1 + numbers.num2;
     format!("Sum: {}", result)
 }
 
+#[get("/health")]
 async fn health_check() -> impl Responder {
     "Ok"
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(web::resource("/health_check").route(web::get().to(health_check)))
-            .service(web::resource("/add").route(web::post().to(add)))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(health_check).service(add))
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
 }
